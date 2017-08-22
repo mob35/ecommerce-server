3b1c0accbf4523e43417beeffbe44415692c8a53
Bu4y <amnes1a@windowslive.com>
2017-08-22 11:30:23 +0700

    menus service

--- modules/products/client/config/products.client.config.js
+++ modules/products/client/config/products.client.config.js
@@ -7,9 +7,9 @@
 
   menuConfig.$inject = ['Menus'];
 
-  function menuConfig(menuService) {
+  function menuConfig(Menus) {
     // Set top bar menu items
-    menuService.addMenuItem('topbar', {
+    Menus.addMenuItem('topbar', {
       title: 'Products',
       state: 'products',
       type: 'dropdown',
@@ -17,13 +17,13 @@
     });
 
     // Add the dropdown list item
-    menuService.addSubMenuItem('topbar', 'products', {
+    Menus.addSubMenuItem('topbar', 'products', {
       title: 'List Products',
       state: 'products.list'
     });
 
     // Add the dropdown create item
-    menuService.addSubMenuItem('topbar', 'products', {
+    Menus.addSubMenuItem('topbar', 'products', {
       title: 'Create Product',
       state: 'products.create',
       roles: ['user']
