3cf2ccdbabe796b99926c3f1aa5c927038d9ddef
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-08-24 15:20:31 +0700

    update

--- modules/manage-carts/server/controllers/manage-carts.server.controller.js
+++ modules/manage-carts/server/controllers/manage-carts.server.controller.js
@@ -65,7 +65,7 @@ exports.processingAddUserCart = function (req, res, next) {
       // return obj.product._id.toString() === product._id.toString();
       if (product.issize && product.issizeselectedsize) {
         if (obj.product._id.toString() === product._id.toString()) {
-          if (obj.issizeselectedsize === product.issizeselectedsize) {
+          if (obj.selectedsize === product.selectedsize) {
             return true;
           }
         }
